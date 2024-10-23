6ed4063f8804b5bb7364442ae31fa3b7ac3e5f16
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-10-23 16:02:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/entities/services/UserService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.entities.services;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return  repository.findAll();
+    }
+
+    public User findById( Integer id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
